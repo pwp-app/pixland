431a56c12daa56d6d98ce9852c40c98afb2e0f51
BackRunner <dev@backrunner.top>
2021-12-27 23:20:22 +0800

    fix: lint error

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
   const configService = app.get<ConfigService>(ConfigService);
   // add validation pipe
   app.useGlobalPipes(
