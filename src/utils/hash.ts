66579a8c3ff4f79d2c47c8f5fc035ae60672db94
BackRunner <dev@backrunner.top>
2022-01-22 23:47:28 +0800

    fix: file key compose

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -2,5 +2,5 @@ import * as crypto from 'crypto';
 
 export const getFileKey = (username: string, password: string) => {
   const hmac = crypto.createHmac('sha256', 'pixland').update(`${username}_${password}`).digest('hex');
-  return crypto.createHmac('sha1', 'pixland').update(hmac).digest('hex');
+  return `userData/${crypto.createHmac('sha1', 'pixland').update(hmac).digest('hex')}.json`;
 };
